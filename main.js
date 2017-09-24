7e63f69dbd10a8aa129bf122efb4e54d3b845253
Kalalau <cantrellkalalau@gmail.com>
2017-09-24 16:21:10 -0400

    commiting

---
+++ main.js
@@ -0,0 +1,9 @@
+tweetGen = {
+	createTweet: function() {
+					var tweetContainer = document.createElement('div');
+					tweetContainer.addClass('container box');
+					tweetContainer.html('<div class="row"><div class="col-md-2 col-sm-2"><img class="profile-pic" src="profile-pic.png"></div><div class="col-md-10 col-sm-10"><h4>Username</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div></div>');
+				 }
+}
+
+// setInterval(tweetGen.createTweet, 6000);
